7423eb1ebda5589188da856ae0d0c9674624a6ba
ElPerenza <pev.cookie@gmail.com>
2022-10-24 23:52:40 +0200

    implemented most of the business logic, need to tidy up code

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { exec } from "child_process"
+import * as fs from "fs"
+import * as path from "path"
+import { promisify } from "util"
+import * as vscode from "vscode"
+
+const execPromise = promisify(exec)
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-serz" is now active!');
+	//register the conversion command
+	let disposable = vscode.commands.registerCommand('vscode-serz.convert', async () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-serz.convert', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSCode-serz!');
-	});
+		//check if the specified serz path in the settings exists
+		let serzPath = vscode.workspace.getConfiguration("vscode-serz").get<string>("serzExePath")
+		if(!fs.existsSync(serzPath)) {
+			let selection = await vscode.window.showErrorMessage(`Path "${serzPath}" isn't a valid serz executable path (file does not exist).`, "Open settings")
+			if(selection === "Open settings") {
+				vscode.commands.executeCommand("workbench.action.openSettings", "vscode-serz.serzExePath")
+			}
+			return
+		}
 
-	context.subscriptions.push(disposable);
+		//activeTextEditor return undefined for large files (supposedly over 50MB), which means that most Tracks.xml files won't open
+		let activeFile = vscode.window.activeTextEditor.document.fileName
+		let fileExt = path.extname(activeFile)
+		let convertedFile: string
+		if(fileExt === ".bin") {
+			convertedFile = activeFile.replace(/(bin)$/, "xml")
+		} else {
+			convertedFile = activeFile.replace(/(xml)$/, "bin")
+		}
+
+		await vscode.window.withProgress({location: vscode.ProgressLocation.Notification}, async (progress) => {
+			progress.report({message: `Converting ${activeFile} to ${fileExt === ".bin" ? ".xml" : ".bin"}`})
+			let {stdout, stderr} = await execPromise(`${serzPath} "${activeFile}"`)
+			if(stderr.length !== 0) {
+				vscode.window.showErrorMessage(`Failed to convert "${activeFile}". Error: ${stderr}`)
+			} else if(!stdout.includes("Conversion complete")) {
+				vscode.window.showErrorMessage(`Failed to convert "${activeFile}". Error: ${stdout}`)
+			} else {
+				vscode.window.showInformationMessage("Conversion complete")
+			}
+		})
+
+		await vscode.commands.executeCommand("vscode.open", vscode.Uri.file(convertedFile))
+	})
+
+	context.subscriptions.push(disposable)
 }
 
 // This method is called when your extension is deactivated
