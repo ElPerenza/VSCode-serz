8c46a584e2fe051e17ab18664929e5f0cb346b60
ElPerenza <pev.cookie@gmail.com>
2022-10-25 21:45:08 +0200

    move conversion logic to separate function, support having a dir path instead of direct serz path in settings + add comments

--- src/extension.ts
+++ src/extension.ts
@@ -12,40 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//register the conversion command
 	let disposable = vscode.commands.registerCommand('vscode-serz.convert', async () => {
-
-		//check if the specified serz path in the settings exists
-		let serzPath = vscode.workspace.getConfiguration("vscode-serz").get<string>("serzExePath")
-		if(!fs.existsSync(serzPath)) {
-			let selection = await vscode.window.showErrorMessage(`Path "${serzPath}" isn't a valid serz executable path (file does not exist).`, "Open settings")
-			if(selection === "Open settings") {
-				vscode.commands.executeCommand("workbench.action.openSettings", "vscode-serz.serzExePath")
-			}
-			return
-		}
-
-		//activeTextEditor return undefined for large files (supposedly over 50MB), which means that most Tracks.xml files won't open
-		let activeFile = vscode.window.activeTextEditor.document.fileName
-		let fileExt = path.extname(activeFile)
-		let convertedFile: string
-		if(fileExt === ".bin") {
-			convertedFile = activeFile.replace(/(bin)$/, "xml")
-		} else {
-			convertedFile = activeFile.replace(/(xml)$/, "bin")
-		}
-
-		await vscode.window.withProgress({location: vscode.ProgressLocation.Notification}, async (progress) => {
-			progress.report({message: `Converting ${activeFile} to ${fileExt === ".bin" ? ".xml" : ".bin"}`})
-			let {stdout, stderr} = await execPromise(`${serzPath} "${activeFile}"`)
-			if(stderr.length !== 0) {
-				vscode.window.showErrorMessage(`Failed to convert "${activeFile}". Error: ${stderr}`)
-			} else if(!stdout.includes("Conversion complete")) {
-				vscode.window.showErrorMessage(`Failed to convert "${activeFile}". Error: ${stdout}`)
-			} else {
-				vscode.window.showInformationMessage("Conversion complete")
-			}
-		})
-
-		await vscode.commands.executeCommand("vscode.open", vscode.Uri.file(convertedFile))
+		await convertThenOpen(vscode.window.activeTextEditor.document.fileName)
 	})
 
 	context.subscriptions.push(disposable)
@@ -53,3 +20,58 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+/**
+ * Uses serz.exe to convert the given file .bin -> .xml or vice versa, then opens the converted file in the editor.
+ * @param filePath the path pointing to the file to convert
+ */
+async function convertThenOpen(filePath: string): Promise<void> {
+	
+	//get the serz.exe path from the settings
+	let serzPath = vscode.workspace.getConfiguration("vscode-serz").get<string>("serzExePath")
+	//if the path set in the settings is a directory, try to search for serz inside that folder
+	if(fs.lstatSync(serzPath).isDirectory()) {
+		serzPath = path.join(serzPath, "serz.exe")
+	}
+	//check if serz exists and, if it's invalid, give the user an option to directly open settings to change it
+	if(!fs.existsSync(serzPath)) {
+		let selection = await vscode.window.showErrorMessage(`Path "${serzPath}" isn't a valid serz executable path (file does not exist).`, "Open settings")
+		if(selection === "Open settings") {
+			vscode.commands.executeCommand("workbench.action.openSettings", "vscode-serz.serzExePath")
+		}
+		return
+	}
+
+	//build the converted file's path
+	let fileExt = path.extname(filePath)
+	let convertedFile: string
+	if(fileExt === ".bin") {
+		convertedFile = filePath.replace(/(bin)$/, "xml")
+	} else {
+		convertedFile = filePath.replace(/(xml)$/, "bin")
+	}
+
+	//convert the file with serz.exe
+	try {
+		await vscode.window.withProgress({location: vscode.ProgressLocation.Notification}, async (progress) => {
+			progress.report({message: `Converting ${filePath} to ${fileExt === ".bin" ? ".xml" : ".bin"}`})
+			//execute serz.exe and throw an error if something went wrong
+			//serz usage: "/path/to/serz.exe file-to-convert.[bin, xml]"
+			let {stdout, stderr} = await execPromise(`${serzPath} "${filePath}"`)
+			if(stderr.length !== 0) {
+				throw new Error(stderr)
+			} else if(!stdout.includes("Conversion complete")) {
+				//serz.exe doesn't write errors to stderr but to stdout for some reason, so we treat any message not containing "Conversion complete" as an error
+				throw new Error(stdout)
+			}
+		})
+	} catch(error) {
+		vscode.window.showErrorMessage(`Failed to convert ${filePath}: ${(error as Error).message}`)
+		return
+	}
+
+	vscode.window.showInformationMessage("Conversion complete")
+	//use this way to open files instead of vscode.workspace.openTextDocument because this method has no 50MB size limit like openTextDocument, 
+	//which is quite easy to exceed when working with RailWorks .xml files
+	await vscode.commands.executeCommand("vscode.open", vscode.Uri.file(convertedFile))
+}
